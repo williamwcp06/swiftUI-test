e34592d9a692e4859a9e59823b3c19a65a4c5b64
william <william.putra@bankneo.co.id>
2022-02-13 12:03:05 +0700

    first commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by William on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
